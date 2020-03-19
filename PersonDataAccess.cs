462c0dc349a4a6dd0129fa7b5dc3fa831c04b4ee
anmol1298 <40059237+anmol1298@users.noreply.github.com>
2020-03-20 00:35:55 +0530

    Add files via upload

---
+++ PersonDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.DatabaseModels;
+using System.Linq;
+
+namespace DataAccess
+{
+    public class PersonDataAccess
+    {
+        public int Create(PersonEntity personEntity)
+        {
+            PersonContext personContext = new PersonContext();
+            personContext.Persons.Add(personEntity);
+            personContext.SaveChanges();
+            return 1;
+
+        }
+
+        public bool Exists(int personId)
+        {
+            PersonContext db = new PersonContext();
+            PersonEntity personEntity = new PersonEntity();
+
+            var person = db.Persons.Where(p => p.Id == personId).FirstOrDefault();
+            if (person != null) // update
+            {
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
